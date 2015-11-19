cd58863e4e318d3fa878ecc8bf4d0bf1a5eb5c8d
Mario Peschel <mp@inwx.de>
2015-11-19 12:08:03 +0100

    Bugfix for more than 1000 domains sync

--- modules/registrars/internetworx/internetworxsync.php
+++ modules/registrars/internetworx/internetworxsync.php
@@ -8,6 +8,9 @@ include_once dirname(__FILE__)."/../../../includes/functions.php";
 include_once dirname(__FILE__)."/../../../includes/registrarfunctions.php";
 include_once "$registrar.php";
 
+$params = getregistrarconfigoptions($registrar);
+$domrobot = new domrobot($params["Username"],$params["Password"],$params["TestMode"]);
+
 $result = full_query("
 	SELECT id, domain, nextduedate, status FROM tbldomains
 	WHERE registrar='$registrar'");
@@ -17,51 +20,58 @@ while ( $row = mysql_fetch_array($result, MYSQL_ASSOC) ) {
 	$data[$row["domain"]] = array("id"=>$row["id"],"nextduedate"=>$row["nextduedate"],"status"=>$row["status"]);
 }
 
-$pDomain['page'] = 0;
-$pDomain['pagelimit'] = 0;
+$pDomain=array();
 $pDomain['domain'] = array_keys($data);
 if (count($pDomain['domain'])<1) {
 	exit;
 }
-$params = getregistrarconfigoptions($registrar);
-$domrobot = new domrobot($params["Username"],$params["Password"],$params["TestMode"]);
-$response = $domrobot->call('domain','list',$pDomain);
-if ($response['code']==1000 && isset($response['resData']['domain']) && $response['resData']['count']>0) {
-	for($i=0;$i<count($response['resData']['domain']);$i++) {
-		$domain = $response['resData']['domain'][$i]['domain'];
-		$status = $response['resData']['domain'][$i]['status'];
-		$exDate = (isset($response['resData']['domain'][$i]['exDate']))?date('Y-m-d',$response['resData']['domain'][$i]['exDate']->timestamp):null;
-		$reDate = (isset($response['resData']['domain'][$i]['reDate']))?date('Y-m-d',$response['resData']['domain'][$i]['reDate']->timestamp):null;
-		
-		if ($status!='OK' || !isset($data[$domain]) || empty($exDate) || empty($reDate)) {
-			continue;
-		}
-		$rowId = $data[$domain]['id'];
-		
-		$whmcsDomainStatus = strtolower($data[$domain]['status']);
-		if ($whmcsDomainStatus!='active' && $status=='OK') {
-			print "$domain - ACTIVE\n";
-			full_query ("
-				UPDATE tbldomains
-				SET status='Active'
-				WHERE id='".db_escape_string($rowId)."'
-				LIMIT 1
-			");
-		}
+$pDomain['pagelimit'] = 1000;
+$page=0;
 
-		$nextDueDate = $data[$domain]['nextduedate'];
-		if ($reDate != $nextDueDate ) {
-			print "$domain - $exDate - $reDate\n";
+do {
+	$page++;
+	$pDomain['page'] = $page;
+
+	$response = $domrobot->call('domain','list',$pDomain);
+	if ($response['code']==1000 && isset($response['resData']['domain']) && $response['resData']['count']>0) {
+		for($i=0;$i<count($response['resData']['domain']);$i++) {
+			$domain = $response['resData']['domain'][$i]['domain'];
+			$status = $response['resData']['domain'][$i]['status'];
+			$exDate = (isset($response['resData']['domain'][$i]['exDate']))?date('Y-m-d',$response['resData']['domain'][$i]['exDate']->timestamp):null;
+			$reDate = (isset($response['resData']['domain'][$i]['reDate']))?date('Y-m-d',$response['resData']['domain'][$i]['reDate']->timestamp):null;
+			
+			if ($status!='OK' || !isset($data[$domain]) || empty($exDate) || empty($reDate)) {
+				continue;
+			}
+			$rowId = $data[$domain]['id'];
+			
+			$whmcsDomainStatus = strtolower($data[$domain]['status']);
+			if ($whmcsDomainStatus!='active' && $status=='OK') {
+				print "$domain - ACTIVE\n";
+				full_query ("
+					UPDATE tbldomains
+					SET status='Active'
+					WHERE id='".db_escape_string($rowId)."'
+					LIMIT 1
+					");
+			}
+
+			$nextDueDate = $data[$domain]['nextduedate'];
+			if ($reDate != $nextDueDate ) {
+				print "$domain - $exDate - $reDate\n";
+			}
 			full_query ("
-				UPDATE tbldomains
-				SET expirydate='".db_escape_string($exDate)."',
-				nextinvoicedate=DATE_ADD(nextinvoicedate, INTERVAL (UNIX_TIMESTAMP('".db_escape_string($reDate)."')-UNIX_TIMESTAMP(nextduedate)) SECOND),
-				nextduedate='".db_escape_string($reDate)."'
-				WHERE nextduedate !='".db_escape_string($reDate)."'
-				AND id='".db_escape_string($rowId)."'
-				LIMIT 1
+					UPDATE tbldomains
+					SET expirydate='".db_escape_string($exDate)."',
+					nextinvoicedate=DATE_ADD(nextinvoicedate, INTERVAL (UNIX_TIMESTAMP('".db_escape_string($reDate)."')-UNIX_TIMESTAMP(nextduedate)) SECOND),
+					nextduedate='".db_escape_string($reDate)."'
+					WHERE nextduedate !='".db_escape_string($reDate)."'
+					AND id='".db_escape_string($rowId)."'
+					LIMIT 1
 			");
 		}
 	}
-}
+	$resCount=count($response['resData']['domain']);
+} while ($resCount==$pDomain['pagelimit']);
+
 ?>
