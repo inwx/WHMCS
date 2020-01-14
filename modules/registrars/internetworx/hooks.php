48dd668cc2eca0168cee1381a4aa4279719e4fe7
Nick Ufer <me@nick-ufer.de>
2020-01-14 11:13:16 +0100

    Fixes formatting with php-cs-fixer

--- modules/registrars/internetworx/hooks.php
+++ modules/registrars/internetworx/hooks.php
@@ -1,7 +1,9 @@
 <?php
-add_hook('ClientAreaPageDomainDNSManagement', 1, "set_dns_entries");
 
-function set_dns_entries($vars) {
-$supported_dns_records['records'] = array('A' => 'A (Address)', 'AAAA' => 'AAAA (Address)', 'MXE' => 'MXE (Mail Easy)', 'MX' => 'MX', 'CNAME' => 'CNAME', 'TXT' => 'TXT (SPF)', 'URL'=>'URL Redirect', 'FRAME' => 'URL FRAME', 'SRV'=>'SRV');
-return $supported_dns_records;
+add_hook('ClientAreaPageDomainDNSManagement', 1, 'set_dns_entries');
+
+function set_dns_entries($vars)
+{
+    $supported_dns_records['records'] = ['A' => 'A (Address)', 'AAAA' => 'AAAA (Address)', 'MXE' => 'MXE (Mail Easy)', 'MX' => 'MX', 'CNAME' => 'CNAME', 'TXT' => 'TXT (SPF)', 'URL' => 'URL Redirect', 'FRAME' => 'URL FRAME', 'SRV' => 'SRV'];
+    return $supported_dns_records;
 }
