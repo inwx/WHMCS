3df85236f11f9933aa311e86d28f379ad0b5ebae
lotionboss-inwx <52823475+ramitinmah-inwx@users.noreply.github.com>
2022-09-26 10:56:31 +0200

    fix: renewal mode of transferred domain is auto expire

--- modules/registrars/internetworx/internetworx.php
+++ modules/registrars/internetworx/internetworx.php
@@ -579,6 +579,7 @@ function internetworx_TransferDomain($params)
 
     // 	Transfer Domain
     $pDomain['domain'] = $params['original']['sld'] . '.' . $params['original']['tld'];
+    $pDomain['renewalMode'] = ($params['tld'] === 'at' || substr($params['tld'], -3) === '.at') ? 'AUTODELETE' : 'AUTOEXPIRE';
     if (isset($params['TechHandle']) && !empty($params['TechHandle'])) {
         $pDomain['tech'] = $params['TechHandle'];
     } else {
